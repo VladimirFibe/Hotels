743c5edb99777b1c5fbd81eef2749dcbe7af103e
Vladimir Fibe <motiw@icloud.com>
2023-12-20 17:59:12 +0600

    about room button RoomView

--- Hotels/Modules/Room/RoomView.swift
+++ Hotels/Modules/Room/RoomView.swift
@@ -5,6 +5,20 @@ struct RoomView: View {
     var body: some View {
         VStack {
             PhotoGalleryView()
+            Text("Стандартный с видном на бассейн или сад")
+                .font(.system(size: 22, weight: .medium))
+            HStack {
+                Text("Подробнее о номере")
+                Image(systemName: "chevron.right")
+            }
+            .font(.system(size: 16, weight: .medium))
+            .foregroundStyle(AppColor.blue.color)
+            .padding(.horizontal, 10)
+            .frame(height: 29)
+            .background(AppColor.blue.color.opacity(0.1))
+            .clipShape(RoundedRectangle(cornerRadius: 5))
+            .frame(maxWidth: .infinity, alignment: .leading)
+
             button
         }
         .hotelSectionModifier()
